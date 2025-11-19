41842b975c4a7974e4d2513331fd8440ffa3c493
Dxrwin <101292131+Dxrwin@users.noreply.github.com>
2025-11-18 21:26:39 -0500

    Delete app directory

--- app/models/modulo.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Text, Boolean
-from db.database import Base
-
-class Modulo(Base):
-    __tablename__ = "modulos"
-    id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String(100), unique=True, nullable=False)
-    descripcion = Column(Text)
-    estado = Column(Boolean, default=True)
--- app/websocket/security.py
+++
@@ -1,8 +0,0 @@
-from jose import jwt, JWTError
-from config.config import settings
-
-def decode_jwt(token: str):
-    try:
-        return jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
-    except JWTError:
-        return None
